d155b2b266ffe7659d8c5143562ce970da1739b3
Adam Johnson <adamwjo@gmail.com>
2018-07-24 14:56:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(sentence)
   str = sentence.split
   array = []
   str.collect do |word|
-    "#{word}".reverse
+    word.reverse
     array.push(word)
   end
-  array.join("")
+  array.join(" ")
 end
